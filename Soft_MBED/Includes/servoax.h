6aa8d5e852ac5830c7942cce8fb0d5826e8d51e0
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2018-10-31 09:17:52 +0100

    #12 Version initiale du driver de servo AX pour MBED

---
+++ Soft_MBED/Includes/servoax.h
@@ -0,0 +1,43 @@
+#ifndef SERVOAX_H
+#define SERVOAX_H
+
+#include "servoaxbase.h"
+
+class ServoAX : public ServoAXBase
+{
+public:
+    ServoAX();
+    ~ServoAX();
+
+    // pure virtual methods to abstract hardware.
+    // to be implemented on specific hardware.
+    virtual tAxErr read(unsigned char *buff_data, unsigned char size);
+    virtual tAxErr write(unsigned char *buff_data, unsigned char size);
+    virtual tAxErr flushSerialInput();
+    virtual tAxErr waitTransmitComplete();
+    virtual tAxErr setTxEnable(bool state);
+    virtual void delay_us(unsigned long delay);
+
+    virtual bool isPresent(unsigned char id);
+    virtual tAxErr setPosition(unsigned char id, unsigned short position);
+    virtual unsigned short getPosition(unsigned char id, tAxErr *err_status=nullptr);
+    virtual tAxErr setSpeed(unsigned char id, unsigned short speed);
+    virtual tAxErr setPositionSpeed(unsigned char id, unsigned short position, unsigned short speed);
+    virtual bool isMoving(unsigned char id, tAxErr *err_status=nullptr);
+    virtual tAxErr changeID(unsigned char old_id, unsigned char new_id);
+    virtual tAxErr enableTorque(unsigned char id, bool on_off=true);
+    virtual tAxErr setMode(unsigned char id, unsigned char mode);
+    virtual tAxErr setLimitPositionMin(unsigned char id, unsigned short pos);
+    virtual tAxErr setLimitPositionMax(unsigned char id, unsigned short pos);
+
+
+
+    tAxErr init();
+
+
+
+private :
+    tAxErr readEEPROM();
+};
+
+#endif // SERVOAX_H
---
+++ Soft_MBED/Sources/servoax.cpp
@@ -0,0 +1,192 @@
+#include "RessourcesHardware.h"
+#include "CGlobale.h"  // pour l'accès au module EEPROM
+#include "servoax.h"
+
+ServoAX::ServoAX()
+{
+    setTxEnable(false);
+    _ax12_serial.baud(1000000);
+}
+
+ServoAX::~ServoAX()
+{
+}
+
+// ============================================================
+//         IMPLEMENTATION DES METHODES VIRTUELLES PURES
+//                  (en lien avec le hardware)
+// ============================================================
+// ______________________________________________________________
+tAxErr ServoAX::read(unsigned char *buff_data, unsigned char size)
+{
+    unsigned long _timeout=0;
+    unsigned char index=0;
+
+    for (_timeout=0; _timeout<0xFFFF; _timeout++) {
+        if (_ax12_serial.readable()) {
+            buff_data[index++] = _ax12_serial.getc();
+            _timeout = 0;
+        }
+        if (index == size) return AX_OK;
+    }
+    return AX_TIMEOUT;
+}
+
+// ______________________________________________________________
+tAxErr ServoAX::write(unsigned char *buff_data, unsigned char size)
+{
+    for (unsigned char i=0; i<size; i++) {
+        _ax12_serial.putc(buff_data[i]);
+    }
+    return AX_OK;
+}
+
+// ______________________________________________________________
+tAxErr ServoAX::flushSerialInput()
+{
+    while (_ax12_serial.readable()) _ax12_serial.getc();  // flush serial input
+    return AX_OK;
+}
+
+// ______________________________________________________________
+tAxErr ServoAX::waitTransmitComplete()
+{
+    //! warning : LPC_UART2 correspond to p27 / p28 MBED UART pins
+    while((LPC_UART2->LSR & 0x00000040) == 0){}  // wait for serial tx fifo empty
+    return AX_OK;
+}
+
+// ______________________________________________________________
+tAxErr ServoAX::setTxEnable(bool state)
+{
+    _Stor3 = state;
+    return AX_OK;
+}
+
+// ______________________________________________________________
+void ServoAX::delay_us(unsigned long delay)
+{
+    wait_us(delay);
+}
+
+// ============================================================
+//           REDIRECTION DES FONCTIONS POUR YAKINDU
+//              (Ticket Itemis 2018102237000178)
+// Problème :
+//   Les méthodes de la classe de base ne sont pas accessibles
+//    depuis le modèle Yakindu.
+// Solution :
+//  Rediriger les fonctions accessibles depuis le modèle vers la
+//    classe de base.
+// ============================================================
+// ____________________________________________________________
+bool ServoAX::isPresent(unsigned char id)
+{
+    return ServoAXBase::isPresent(id);
+}
+
+// ____________________________________________________________
+tAxErr ServoAX::setPosition(unsigned char id, unsigned short position)
+{
+    return ServoAXBase::setPosition(id, position);
+}
+
+// ____________________________________________________________
+tAxErr ServoAX::setSpeed(unsigned char id, unsigned short speed)
+{
+   return ServoAXBase::setSpeed(id, speed);
+}
+
+// ____________________________________________________________
+tAxErr ServoAX::setPositionSpeed(unsigned char id, unsigned short position, unsigned short speed)
+{
+    return ServoAXBase::setPositionSpeed(id, position, speed);
+}
+
+// ____________________________________________________________
+unsigned short ServoAX::getPosition(unsigned char id, tAxErr *err_status)
+{
+    return ServoAXBase::getPosition(id, err_status);
+}
+
+// ____________________________________________________________
+bool ServoAX::isMoving(unsigned char id, tAxErr *err_status)
+{
+    return ServoAXBase::isMoving(id, err_status);
+}
+
+// ____________________________________________________________
+tAxErr ServoAX::changeID(unsigned char old_id, unsigned char new_id)
+{
+    return ServoAXBase::changeID(old_id, new_id);
+}
+
+// ____________________________________________________________
+tAxErr ServoAX::enableTorque(unsigned char id, bool on_off)
+{
+    return ServoAXBase::enableTorque(id, on_off);
+}
+
+// ____________________________________________________________
+tAxErr ServoAX::setMode(unsigned char id, unsigned char mode)
+{
+    return ServoAXBase::setMode(id, mode);
+}
+
+// ____________________________________________________________
+tAxErr ServoAX::setLimitPositionMin(unsigned char id, unsigned short pos)
+{
+    return ServoAXBase::setLimitPositionMin(id, pos);
+}
+
+// ____________________________________________________________
+tAxErr ServoAX::setLimitPositionMax(unsigned char id, unsigned short pos)
+{
+    return ServoAXBase::setLimitPositionMax(id, pos);
+}
+
+
+
+
+
+// ______________________________________________________________
+tAxErr ServoAX::init(void)
+{
+    tAxErr err;
+    err = readEEPROM(); // Lit l'EEPROM et configure le module
+    return err;
+}
+
+// ______________________________________________________________
+tAxErr ServoAX::readEEPROM(void)
+{
+    tAxErr err;
+    // _________________________
+    // Lecture des parmaètres de configuration des servos dans l'EEPROM :
+    //    - Butée min
+    //    - Butée max
+    //    - Position initiale à la mise sous tension
+    char sBuff[64]="\0";
+    unsigned int servo_val;
+    for (unsigned int i=0; i<NBRE_SERVOS_AX; i++) {
+        sprintf(sBuff, "butee_min_servo_ax_%d", i);
+        if (Application.m_eeprom.getValue(sBuff, &servo_val)) {
+            err = setLimitPositionMin(i, servo_val);
+            if (err != AX_OK) return err;
+        }
+
+        sprintf(sBuff, "butee_max_servo_ax_%d", i);
+        if (Application.m_eeprom.getValue(sBuff, &servo_val)) {
+            err = setLimitPositionMax(i, servo_val);
+            if (err != AX_OK) return err;
+        }
+
+        sprintf(sBuff, "position_initiale_servo_ax_%d", i);
+        if (Application.m_eeprom.getValue(sBuff, &servo_val)) {
+            err = setPosition(i, servo_val);
+            if (err != AX_OK) return err;
+            wait (0.4); // Tempo pour être certain que tous les servos ne bougent pas en même temps (surconsommation)
+        }
+    }
+    return AX_OK;
+}
