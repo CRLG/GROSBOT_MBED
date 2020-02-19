30fbf28a75ad9d976d4bc306a9cc38aab1d5626e
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2020-02-19 17:37:11 +0100

    #49 Force l'arrêt de l'ascenseur en fin de match

--- Soft_MBED/Modelia/sm_outputsinterface.cpp
+++ Soft_MBED/Modelia/sm_outputsinterface.cpp
@@ -16,5 +16,6 @@ void SM_OutputsInterface::stopAll()
     // TODO
     // Arrêter tous les moteurs, servos et autres actionneurs
     Application.m_asservissement.CommandeManuelle(0, 0);
+    Application.m_asservissement_chariot.Stop_Chariot();
     Application.m_power_electrobot.setAllOutputs(0);
 }
