a67c6a22f9f17d4e3cb07516bc8198794f233f5b
laguiche <ste.guichard@gmail.com>
2020-04-08 11:57:19 +0200

    CDR2020 - stratégie de récupération des 2 (voire 3) bouées de départ avec déplacements

--- Soft_MBED/Modelia/sm_recup2boueeszonedepart.cpp
+++ Soft_MBED/Modelia/sm_recup2boueeszonedepart.cpp
@@ -1,77 +1,122 @@
-#include "CGlobale.h"
+/**
+ * Generated 08_04_2020 at 11_38
+ */
+
 #include "sm_recup2boueeszonedepart.h"
+#include "CGlobale.h"
 
 SM_Recup2BoueesZoneDepart::SM_Recup2BoueesZoneDepart()
 {
-    m_main_mission_type = true;
+	m_main_mission_type = true;
+	m_max_score = 0;
 }
 
 const char* SM_Recup2BoueesZoneDepart::getName()
 {
-    return "SM_Recup2BoueesZoneDepart";
+	return "SM_Recup2BoueesZoneDepart";
 }
 
 const char* SM_Recup2BoueesZoneDepart::stateToName(unsigned short state)
 {
-    switch(state)
-    {
-    //case ALLER_VERS_PHARE :         return "ALLER_VERS_PHARE";
-    }
-    return "UNKNOWN_STATE";
+	switch(state)
+	{
+		case STATE_1 :		return "STATE_1";
+		case STATE_2 :		return "STATE_2";
+		case STATE_3 :		return "STATE_3";
+		case STATE_4 :		return "STATE_4";
+		case STATE_5 :		return "STATE_5";
+		case STATE_6 :		return "STATE_6";
+		case STATE_7 :		return "STATE_7";
+		case STATE_8 :		return "STATE_8";
+		case FIN_MISSION :	return "FIN_MISSION";
+	}
+	return "UNKNOWN_STATE";
 }
-
 
 // _____________________________________
 void SM_Recup2BoueesZoneDepart::step()
 {
-    switch (m_state)
-    {
-    // ___________________________
-    case STATE_1:
-        if (onEntry()) {
-            Application.m_power_electrobot.setOutput((dsPicPowerElectrobotBase::tSwitchOutput)EV_AV, 1);
-        }
+	switch (m_state)
+	{
 
-        gotoStateAfter(STATE_2, 900);
+	// ___________________________
+	case STATE_1 :
+		if (onEntry()) {
+			Application.m_asservissement.CommandeMouvementXY_TETA(60,2,0.3);/**/
+		}
 
-        if (onExit()) { }
-        break;
-    // ___________________________
-    case STATE_2:
-        if (onEntry()) {
-            Application.m_power_electrobot.setOutput((dsPicPowerElectrobotBase::tSwitchOutput)EV_AV, 0);
-        }
+			gotoStateIfConvergence(STATE_2,5000);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_2 :
+		if (onEntry()) {
+			Application.m_asservissement.CommandeMouvementXY_TETA(128,23,0);/*TODO profiter d'etre dans le coin pour reconnaitre nord-sud*/
+		}
 
-        gotoStateAfter(STATE_1, 1600);
+			gotoStateIfConvergence(STATE_3,5000);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_3 :
+		if (onEntry()) {
+			Application.m_asservissement.CommandeMouvementXY_TETA(30,23,0);/**/
+		}
 
-        if (onExit()) { }
-        break;
-    // ___________________________
-    case STATE_3:
-        if (onEntry()) {
-        }
+			gotoStateIfConvergence(STATE_4,5000);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_4 :
+		if (onEntry()) {
+			Application.m_asservissement.CommandeMouvementXY_TETA(40,23,1.57);/**/
+		}
 
-        gotoStateAfter(STATE_4, 2200);
+			gotoStateIfConvergence(STATE_5,5000);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_5 :
+		if (onEntry()) {
+			Application.m_asservissement.CommandeMouvementXY_TETA(40,45,0);/**/
+		}
 
-        if (onExit()) { }
-        break;
+			gotoStateIfConvergence(STATE_6,5000);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_6 :
+		if (onEntry()) {
+			Application.m_asservissement.CommandeMouvementXY_TETA(10,45,1.57);/**/
+		}
 
-    // ___________________________
-    case STATE_4:
-        if (onEntry()) {
-        }
+			gotoStateIfConvergence(STATE_7,5000);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_7 :
+		if (onEntry()) {
+			Application.m_asservissement.CommandeMouvementXY_TETA(10,30,1.57);/**/
+		}
 
-        gotoStateAfter(FIN_MISSION, 1000);
+			gotoStateIfConvergence(STATE_8,5000);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_8 :
+		if (onEntry()) {
+			Application.m_asservissement.CommandeMouvementXY_TETA(10,40,1.57);/*TODO ranger les actionneurs*/
+		}
 
-        if (onExit()) { }
+			gotoStateIfConvergence(FIN_MISSION,5000);
+		if (onExit()) {  }
+		break;
 
-        break;
-
-    // ___________________________
-    case FIN_MISSION :
-        m_succes = true;
-        m_score = m_max_score;
-        stop();
-        break;
-    }
+	// ___________________________
+	case FIN_MISSION :
+		m_succes = true;
+		m_score = m_max_score;
+		stop();
+		break;
+	}
 }
--- Soft_MBED/Modelia/sm_recup2boueeszonedepart.h
+++ Soft_MBED/Modelia/sm_recup2boueeszonedepart.h
@@ -1,3 +1,7 @@
+/**
+ * Generated 08_04_2020 at 11_38
+ */
+
 #ifndef SM_RECUP2BOUEESZONEDEPART_H
 #define SM_RECUP2BOUEESZONEDEPART_H
 
@@ -6,30 +10,36 @@
 class SM_Recup2BoueesZoneDepart : public SM_StateMachineBase
 {
 public:
-    SM_Recup2BoueesZoneDepart();
+	SM_Recup2BoueesZoneDepart();
+	void step();
+	const char* getName();
+	const char* stateToName(unsigned short state);
 
-    void step();
-    const char* getName();
-    const char* stateToName(unsigned short state);
-
-    typedef enum {
-        STATE_1 = SM_StateMachineBase::SM_FIRST_STATE,
-        STATE_2,
-        STATE_3,
-        STATE_4,
-        STATE_5,
-        STATE_6,
-        STATE_7,
-        STATE_8,
-        STATE_9,
-        STATE_10,
-        STATE_11,
-        STATE_12,
-        STATE_13,
-        STATE_14,
-        STATE_15,
-        FIN_MISSION
-    }tState;
+	typedef enum {
+	STATE_1 = SM_StateMachineBase::SM_FIRST_STATE,
+	STATE_2,
+	STATE_3,
+	STATE_4,
+	STATE_5,
+	STATE_6,
+	STATE_7,
+	STATE_8,
+	STATE_9,
+	STATE_10,
+	STATE_11,
+	STATE_12,
+	STATE_13,
+	STATE_14,
+	STATE_15,
+	STATE_16,
+	STATE_17,
+	STATE_18,
+	STATE_19,
+	STATE_20,
+	STATE_21,
+	STATE_22,
+	FIN_MISSION
+	}tState;
 };
 
 #endif // SM_RECUP2BOUEESZONEDEPART_H
