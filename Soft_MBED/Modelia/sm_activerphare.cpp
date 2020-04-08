7af3746d9012df6c4670e44f8c78de188210bbd3
laguiche <ste.guichard@gmail.com>
2020-04-08 11:56:35 +0200

    CDR2020 - stratégie d'activation phare avec déplacements

--- Soft_MBED/Modelia/sm_activerphare.cpp
+++ Soft_MBED/Modelia/sm_activerphare.cpp
@@ -1,81 +1,72 @@
-#include "sm_activerphare.h"
+/**
+ * Generated 08_04_2020 at 11_37
+ */
 
+#include "sm_activerphare.h"
+#include "CGlobale.h"
 
 SM_ActiverPhare::SM_ActiverPhare()
 {
-    m_main_mission_type = true;
+	m_main_mission_type = true;
+	m_max_score = 0;
 }
 
 const char* SM_ActiverPhare::getName()
 {
-    return "SM_ActiverPhare";
+	return "SM_ActiverPhare";
 }
 
 const char* SM_ActiverPhare::stateToName(unsigned short state)
 {
-    switch(state)
-    {
-    case ALLER_VERS_PHARE :         return "ALLER_VERS_PHARE";
-    case ACCOSTER_PHARE :           return "ACCOSTER_PHARE";
-    case ACTIVER_PHARE :            return "ACTIVER_PHARE";
-    case VERIFIER_PHARE_ALLUME :    return "VERIFIER_PHARE_ALLUME";
-    case FIN_MISSION_PHARE :        return "FIN_MISSION_PHARE";
-    }
-    return "UNKNOWN_STATE";
+	switch(state)
+	{
+		case STATE_1 :		return "STATE_1";
+		case ACTIVER_PHARE :		return "ACTIVER_PHARE";
+		case VERIFIER_PHARE :		return "VERIFIER_PHARE";
+		case FIN_MISSION :	return "FIN_MISSION";
+	}
+	return "UNKNOWN_STATE";
 }
 
+// _____________________________________
 void SM_ActiverPhare::step()
 {
-    switch (m_state)
-    {
-    // ___________________________
-    case ALLER_VERS_PHARE:
-        if (onEntry()) {
-        }
+	switch (m_state)
+	{
 
-        gotoStateAfter(ACCOSTER_PHARE, 1000);
+	// ___________________________
+	case STATE_1 :
+		if (onEntry()) {
+			Application.m_asservissement.CommandeMouvementXY_TETA(10,45,-1.57);/**/
+		}
 
-        if (onExit()) { }
-        break;
-    // ___________________________
-    case ACCOSTER_PHARE:
-        if (onEntry()) {
-        }
+			gotoStateIfConvergence(ACTIVER_PHARE,5000);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case ACTIVER_PHARE :
+		if (onEntry()) {
+			Application.m_asservissement.CommandeMouvementXY_TETA(10,53,-1.57);/**/
+		}
 
-        gotoStateAfter(ACTIVER_PHARE, 1000);
+			gotoStateIfConvergence(VERIFIER_PHARE,5000);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case VERIFIER_PHARE :
+		if (onEntry()) {
+			Application.m_asservissement.CommandeMouvementXY_TETA(10,45,-1.57);/**/
+		}
 
-        if (onExit()) { }
-        break;
-    // ___________________________
-    case ACTIVER_PHARE:
-        if (onEntry()) {
-        }
+			gotoStateIfConvergence(FIN_MISSION,5000);
+		if (onExit()) {  }
+		break;
 
-        gotoStateAfter(VERIFIER_PHARE_ALLUME, 1000);
-
-        if (onExit()) { }
-        break;
-
-    // ___________________________
-    case VERIFIER_PHARE_ALLUME:
-        if (onEntry()) {
-        }
-
-        gotoStateAfter(FIN_MISSION_PHARE, 5000);
-
-        if (onExit()) { }
-
-        break;
-
-    // ___________________________
-    case FIN_MISSION_PHARE :
-        if (onEntry()) { }
-
-        m_succes = true;
-        m_score = m_max_score;
-        stop();
-
-        if (onExit()) { }
-        break;
-    }
+	// ___________________________
+	case FIN_MISSION :
+		m_succes = true;
+		m_score = m_max_score;
+		stop();
+		break;
+	}
 }
--- Soft_MBED/Modelia/sm_activerphare.h
+++ Soft_MBED/Modelia/sm_activerphare.h
@@ -1,25 +1,42 @@
+/**
+ * Generated 08_04_2020 at 11_37
+ */
+
 #ifndef SM_ACTIVERPHARE_H
 #define SM_ACTIVERPHARE_H
 
 #include "sm_statemachinebase.h"
 
-
 class SM_ActiverPhare : public SM_StateMachineBase
 {
 public:
-    SM_ActiverPhare();
+	SM_ActiverPhare();
+	void step();
+	const char* getName();
+	const char* stateToName(unsigned short state);
 
-    void step();
-    const char* getName();
-    const char* stateToName(unsigned short state);
-
-    typedef enum {
-        ALLER_VERS_PHARE = SM_StateMachineBase::SM_FIRST_STATE,
-        ACCOSTER_PHARE,
-        ACTIVER_PHARE,
-        VERIFIER_PHARE_ALLUME,
-        FIN_MISSION_PHARE
-    }tState;
+	typedef enum {
+	STATE_1 = SM_StateMachineBase::SM_FIRST_STATE,
+	STATE_2,
+	STATE_3,
+	STATE_4,
+	STATE_5,
+	STATE_6,
+	STATE_7,
+	STATE_8,
+	STATE_9,
+	STATE_10,
+	STATE_11,
+	STATE_12,
+	STATE_13,
+	STATE_14,
+	STATE_15,
+	STATE_16,
+	STATE_17,
+	ACTIVER_PHARE,
+	VERIFIER_PHARE,
+	FIN_MISSION
+	}tState;
 };
 
 #endif // SM_ACTIVERPHARE_H
