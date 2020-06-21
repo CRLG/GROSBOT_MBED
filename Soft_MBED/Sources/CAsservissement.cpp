19ec810a29fc5993ea3062aff7fc159fe8f7c4ba
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2020-06-21 22:39:00 +0200

    #34: Pas de doublons d'init entre CAsservissement et CAsservissementBase
    Les données communes initialisées dans CASservissementBase ne sont pas réinitialisées dans CAsservissement au risque d'ambiguité.

--- Soft_MBED/Sources/CAsservissement.cpp
+++ Soft_MBED/Sources/CAsservissement.cpp
@@ -43,6 +43,8 @@ void CAsservissement::Init(void)
 {
  int i;
  
+ CAsservissementBase::Init();
+
  // initialisation des paramètres de l'asservissement
  cde_max = 100;				// %	Commande maximum normalisée pour saturer la régulation
  cde_min = -100 ;			// %	Commande minimum normalisée pour saturer la régulation
@@ -59,55 +61,6 @@ void CAsservissement::Init(void)
  zone_morte_D = 12;
  zone_morte_G = 12;
 
- // Les variables de l'asservissement
- distance_consigne = 0;
- angle_consigne = 0;
- distance_roue_D = 0;
- distance_roue_G = 0;
- erreur_distance = 0;
- erreur_angle = 0;
- erreur_vitesse_avance = 0;
- erreur_vitesse_rotation = 0;
- int_terme_prec_distance = 0;
- int_terme_distance = 0;
- cde_distance_ulim = 0;
- cde_distance_recal = 0;
- cde_distance = 0;
- int_terme_prec_angle = 0;
- int_terme_angle = 0;
- cde_angle_ulim = 0;
- cde_angle = 0;
- cde_moteur_D = 0;
- cde_moteur_D_ulim = 0;
- cde_moteur_G = 0;
- cde_moteur_G_ulim = 0;
- angle_robot_prec = 0;
- distance_roue_D_prec = 0;
- distance_roue_G_prec = 0;
- X_robot_prec = 0;
- Y_robot_prec = 0;
- ddistance_roue_D = 0;
- ddistance_roue_G = 0;
- ddistance_robot = 0;
- distance_robot = 0;
- dangle_robot = 0;
- angle_robot = 0;
- dX_robot = 0;
- dY_robot = 0;
- X_robot = 0;
- X_consigne = 0;
- Y_robot = 0;
- X_erreur = 0;
- Y_erreur = 0;
- Y_consigne = 0;
- temp_cos_angle_robot = 0;
- temp_sin_angle_robot = 0;
- Xa = 0;
- Ya = 0;
- Xb = 0;
- Yb = 0;
- Offset_a = 0;
- Offset_b = 0;
  cde_offset_min_D = zone_morte_D;
  cde_offset_min_G = zone_morte_G;
  correction_ubatt = 1;
@@ -116,43 +69,15 @@ void CAsservissement::Init(void)
  m_tension_batterie_filt = 1.44;
  m_tension_batterie_filt_prec = 14.4;
  k_batt = 0.9;							// Coeff de filtrage de la tension batterie (1 = pas de filtrage)
- vitesse_avance_robot = 0;
- vitesse_rotation_robot = 0;
- vitesse_avance_robot_filt_prec = 0;
- vitesse_avance_robot_filt = 0;
  k_filt_avance = 0.7;					// Coeff de filtrage de la vitesse d'avance (1 = pas de filtrage)
- vitesse_rotation_robot_filt_prec = 0;
- vitesse_rotation_robot_filt = 0;
  k_filt_rotation = 0.8;					// Coeff de filtrage de la vitesse de rotation (1 = pas de filtrage)
- consigne_vitesse_avance = 0;
- consigne_vitesse_rotation = 0;
- consigne_vitesse_avance_filt = 0;
- consigne_vitesse_avance_recal = 0;
- consigne_vitesse_rotation_cm = 0;
- consigne_vitesse_rotation_filt = 0;
- facteur_correction_avance_angle = 0;
  seuil_vitesse_diag_blocage = 2;		// cm/s
  commande_min_diag_blocage = 38;		// En cas de blocage l'égrateur charge rapidement à des valeurs importantes
  seuil_vitesse_diag_rotation = 0.1;	// rad/s
  seuil_max_compteur_diag_blocage = 500;	// k * te = g secondes
  inc_diag_blocage = 1;
  dec_diag_blocage = 2;
- compteur_diag_blocage = 0;
- diag_blocage = 0;
- convergence = 0;
- convergence_conf = 0;					// 0: Mvt en cours, 1: Convergence ok, 2: Blocage détecté
- saturation_distance = 0;
- saturation_angle = 0;
- saturation_moteur_D = 0;
- saturation_moteur_G = 0;
- verouillage_approche = 0;
- compteur_convergence = 0;
- ModeAsservissement = cMODE_MANUEL;
- TETA_consigne=0;
- convergence_rapide = 0;
 
- offset_vitesse_avance = 0;				//	[cm/s-2]
- offset_vitesse_rotation = 0;			//	[rad/s-2]
  vitesse_avance_max = 80;				//	[cm/s]
  vitesse_rotation_max = 3;				//	[rad/s]
  Ind_perfo = 0.7;
