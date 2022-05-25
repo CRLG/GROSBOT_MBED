5d942b4fa096d57ef61a98666e5c0ca0e828445e
laguiche <ste.guichard@gmail.com>
2022-05-25 22:51:49 +0200

    implémentation d'un retour en zone de départ

--- Soft_MBED/Modelia/sm_retourzonedepart.cpp
+++ Soft_MBED/Modelia/sm_retourzonedepart.cpp
@@ -1,73 +1,62 @@
+/**
+ * Generated 25_05_2022 at 09_20
+ */
+
 #include "sm_retourzonedepart.h"
+#include "CGlobale.h"
 
 SM_RetourZoneDepart::SM_RetourZoneDepart()
 {
-    m_main_mission_type = true;
+	m_main_mission_type = true;
+	m_max_score = 0;
 }
 
 const char* SM_RetourZoneDepart::getName()
 {
-    return "SM_RetourZoneDepart";
+	return "SM_RetourZoneDepart";
 }
 
 const char* SM_RetourZoneDepart::stateToName(unsigned short state)
 {
-    switch(state)
-    {
-    //case XXX :         return "XXX";
-    }
-    return "UNKNOWN_STATE";
+	switch(state)
+	{
+		case STATE_1 :		return "STATE_1";
+		case STATE_2 :		return "STATE_2";
+		case FIN_MISSION :	return "FIN_MISSION";
+	}
+	return "UNKNOWN_STATE";
 }
 
-
+// _____________________________________
 void SM_RetourZoneDepart::step()
 {
-    switch (m_state)
-    {
-    // ___________________________
-    case STATE_1:
-        if (onEntry()) {
-        }
+	switch (m_state)
+	{
 
-        gotoStateAfter(STATE_2, 3000);
+	// ___________________________
+	case STATE_1 :
+		if (onEntry()) {
+			outputs()->CommandeMouvementXY_TETA_sym(55,5,3.14);/**/
+		}
 
-        if (onExit()) { }
-        break;
-    // ___________________________
-    case STATE_2:
-        if (onEntry()) {
-        }
+			gotoStateIfConvergence(STATE_2,5000);
+		if (onExit()) {  }
+		break;
+	// ___________________________
+	case STATE_2 :
+		if (onEntry()) {
+			outputs()->CommandeMouvementXY_sym(19,5);/**/
+		}
 
-        gotoStateAfter(STATE_3, 2300);
+			gotoStateIfConvergence(FIN_MISSION,5000);
+		if (onExit()) {  }
+		break;
 
-        if (onExit()) { }
-        break;
-    // ___________________________
-    case STATE_3:
-        if (onEntry()) {
-        }
-
-        gotoStateAfter(STATE_4, 1200);
-
-        if (onExit()) { }
-        break;
-
-    // ___________________________
-    case STATE_4:
-        if (onEntry()) {
-        }
-
-        gotoStateAfter(FIN_MISSION, 4000);
-
-        if (onExit()) { }
-
-        break;
-
-    // ___________________________
-    case FIN_MISSION :
-        m_succes = true;
-        m_score = m_max_score;
-        stop();
-        break;
-    }
+	// ___________________________
+	case FIN_MISSION :
+		m_succes = true;
+		m_score = m_max_score;
+		stop();
+		break;
+	}
 }
--- Soft_MBED/Modelia/sm_retourzonedepart.h
+++ Soft_MBED/Modelia/sm_retourzonedepart.h
@@ -1,35 +1,39 @@
-#ifndef SM_RETOUR_ZONE_DEPART_H
-#define SM_RETOUR_ZONE_DEPART_H
+/**
+ * Generated 25_05_2022 at 09_20
+ */
+
+#ifndef SM_RETOURZONEDEPART_H
+#define SM_RETOURZONEDEPART_H
 
 #include "sm_statemachinebase.h"
 
 class SM_RetourZoneDepart : public SM_StateMachineBase
 {
 public:
-    SM_RetourZoneDepart();
+	SM_RetourZoneDepart();
+	void step();
+	const char* getName();
+	const char* stateToName(unsigned short state);
 
-    void step();
-    const char* getName();
-    const char* stateToName(unsigned short state);
-
-    typedef enum {
-        STATE_1 = SM_StateMachineBase::SM_FIRST_STATE,
-        STATE_2,
-        STATE_3,
-        STATE_4,
-        STATE_5,
-        STATE_6,
-        STATE_7,
-        STATE_8,
-        STATE_9,
-        STATE_10,
-        STATE_11,
-        STATE_12,
-        STATE_13,
-        STATE_14,
-        STATE_15,
-        FIN_MISSION
-    }tState;
+	typedef enum {
+	STATE_1 = SM_StateMachineBase::SM_FIRST_STATE,
+	STATE_2,
+	STATE_3,
+	STATE_4,
+	STATE_5,
+	STATE_6,
+	STATE_7,
+	STATE_8,
+	STATE_9,
+	STATE_10,
+	STATE_11,
+	STATE_12,
+	STATE_13,
+	STATE_14,
+	STATE_15,
+	STATE_16,
+	FIN_MISSION
+	}tState;
 };
 
-#endif // SM_RETOUR_ZONE_DEPART_H
+#endif // SM_RETOURZONEDEPART_H
